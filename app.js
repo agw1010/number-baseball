19aead33617e9b0d684982199b2f5c4b0c9ea105
agw1010 <42842700+agw1010@users.noreply.github.com>
2019-05-30 00:16:59 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const strikeOrBall = document.querySelector("strikeOrBall");
+
+const guessField = document.querySelector(".guessField");
+const guessSubmit = document.querySelector(".guessSubmit");
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+
+}
