35d8602ae0738e4ce9f0ac92cee1e8a236f545ea
agw1010 <42842700+agw1010@users.noreply.github.com>
2019-06-02 23:45:12 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let number = [];
+
 const strikeOrBall = document.querySelector("strikeOrBall");
 
 const guessField = document.querySelector(".guessField");
@@ -7,5 +10,5 @@ let guessCount = 1;
 let resetButton;
 
 function checkGuess() {
-
+    let
 }
