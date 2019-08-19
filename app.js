b587852237d39e6d5a0b16fac6c90c0855f00214
agw1010 <gunnwook02@gmail.com>
2019-08-20 01:26:28 +0900

    1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let number = [];
+
 for (let i = 0; i < 3; i++) {
-    let select = Math.floor(Math.random() * list.length);
-    number[i] = list.splice(select, 1)[0];
+  let select = Math.floor(Math.random() * list.length);
+  number[i] = list.splice(select, 1)[0];
 }
+
+const form = document.querySelector(".form");
 const strikeOrBall = document.querySelector(".strikeOrBall");
 const guessField = document.querySelector(".guessField");
 const guessSubmit = document.querySelector(".guessSubmit");
@@ -14,68 +17,68 @@ let resetButton;
 let chanse = 8;
 
 function checkGuess() {
-    let userGuess = guessField.value;
-    let userGuessArray = userGuess.split('');
-    for (let j = 0; j < 3; j++) {
-        for (let k = 0; k < 3; k++) {
-            if (number[j] == userGuessArray[k]) {
-                if (j === k) {
-                    strike++;
-                } else {
-                    ball++;
-                }
-                break;
-            }
+  let userGuess = guessField.value;
+  let userGuessArray = userGuess.split("");
+  for (let j = 0; j < 3; j++) {
+    for (let k = 0; k < 3; k++) {
+      if (number[j] == userGuessArray[k]) {
+        if (j === k) {
+          strike++;
+        } else {
+          ball++;
         }
+        break;
+      }
     }
-    if (strike === 3) {
-        strikeOrBall.textContent = `축하합니다 ${guessCount}번에 맞추셨습니다`;
-        setGameOver();
-    } else if (guessCount === 9) {
-        strikeOrBall.textContent = `!!!GAME OVER!!! 정답은 ${number} 입니다`;
-        setGameOver();
-    } else {
-        strikeOrBall.textContent = `strike : ${strike} ball : ${ball} 기회가 ${chanse}번 남았습니다`
-        strike = 0;
-        ball = 0;
-    }
-    guessCount++;
-    chanse--;
-    guessField.value = '';
-    guessField.focus();
+  }
+  if (strike === 3) {
+    strikeOrBall.textContent = `축하합니다 ${guessCount}번에 맞추셨습니다`;
+    setGameOver();
+  } else if (guessCount === 9) {
+    strikeOrBall.textContent = `!!!GAME OVER!!! 정답은 ${number} 입니다`;
+    setGameOver();
+  } else {
+    strikeOrBall.textContent = `strike : ${strike} ball : ${ball} 기회가 ${chanse}번 남았습니다`;
+    strike = 0;
+    ball = 0;
+  }
+  guessCount++;
+  chanse--;
+  guessField.value = "";
+  guessField.focus();
 }
-guessSubmit.addEventListener('click', checkGuess);
+guessSubmit.addEventListener("click", checkGuess);
 
 function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = '다시하기';
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', resetGame);
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement("button");
+  resetButton.textContent = "🔄";
+  form.appendChild(resetButton);
+  resetButton.addEventListener("click", resetGame);
 }
 
 function resetGame() {
-    guessCount = 1;
+  guessCount = 1;
 
-    const resetParas = document.querySelectorAll('.resultParas p');
-    for (let i = 0; i < resetParas.length; i++) {
-        resetParas[i].textContent = '';
-    }
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = "";
+  }
 
-    resetButton.parentNode.removeChild(resetButton);
+  resetButton.parentNode.removeChild(resetButton);
 
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = '';
-    guessField.focus();
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
 
-    let number = [];
-    for (let i = 0; i < 3; i++) {
-        let select = Math.floor(Math.random() * list.length);
-        number[i] = list.splice(select, 1)[0];
-    }
-    strike = 0;
-    ball = 0;
-    chanse =8;
+  let number = [];
+  for (let i = 0; i < 3; i++) {
+    let select = Math.floor(Math.random() * list.length);
+    number[i] = list.splice(select, 1)[0];
+  }
+  strike = 0;
+  ball = 0;
+  chanse = 8;
 }
